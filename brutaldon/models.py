5c48180a7c74c8506a59019759f5be2de8f5ff59
Jason McBrayer <jmcbray@carcosa.net>
2018-04-24 12:55:44 -0400

    Add client field to Account

--- brutaldon/models.py
+++ brutaldon/models.py
@@ -3,7 +3,7 @@ from django.conf import settings
 
 class Client(models.Model):
     name = models.TextField(default = "brutaldon")
-    api_base_id = models.URLField(default="mastodon.social")
+    api_base_id = models.URLField(default="https://mastodon.social")
     client_id = models.TextField(null=True, blank=True)
     client_secret = models.TextField(null=True, blank=True)
 
@@ -14,3 +14,4 @@ class Account(models.Model):
     username = models.CharField(max_length=80)
     django_user = models.ForeignKey(settings.AUTH_USER_MODEL, models.CASCADE, null=True)
     access_token = models.TextField(null=True, blank=True)
+    client= models.ForeignKey(Client, models.SET_NULL, null=True)
