63cea9be145d12a64964d3d6a9a161465727b65d
Jason McBrayer <jmcbray@carcosa.net>
2018-05-07 08:46:59 -0400

    Link in tags to tag timeline.
    Currently, only tags in toots from Mastodon servers are relinked,
    because they use a consistent and easy-to-support heuristic.

---
+++ brutaldon/templatetags/taglinks.py
@@ -0,0 +1,25 @@
+from django import template
+from bs4 import BeautifulSoup
+from urllib import parse
+from django.urls import reverse
+
+register = template.Library()
+
+@register.filter
+def relink_tags(value):
+    '''Treat the text as html, and replace tag links with app-internal tag links
+
+    Currently, this only works for tags in toots coming from Mastodon servers,
+    not necessarily GNU Social, Pleroma, or other fediverse servers, because
+    it relies on the markup that Mastodon puts on tags.
+
+    FIXME: handle arbitrary tag lengths.
+    '''
+    soup = BeautifulSoup(value, 'html.parser')
+    for link in soup.find_all('a', class_='hashtag'):
+        link['href'] = reverse('tag', args=[link.span.string])
+    return str(soup)
+
+@register.filter
+def relink_toot(value):
+    return relink_tags(value)
