127f8fa261879ef38f9b6ef31cbf79ebc3ced85a
Jason McBrayer <jmcbray@carcosa.net>
2018-06-18 21:33:41 -0400

    Setup for themes and related options

--- brutaldon/models.py
+++ brutaldon/models.py
@@ -10,8 +10,22 @@ class Client(models.Model):
     def __str__(self):
         return self.name + ": " + self.api_base_id
 
+class Theme(models.Model):
+    name = models.TextField(max_length=80)
+    main_css = models.TextField(max_length=1024, blank=True, null=True,
+                                default="css/fullbrutalism.css")
+    tweaks_css = models.TextField(max_length=1024, blank=True, null=True)
+    is_brutalist = models.BooleanField(default=False)
+
+class Preference(models.Model):
+    theme = models.ForeignKey(Theme, models.SET_NULL, null=True)
+    data_saver = models.BooleanField(default=False)
+    fix_emojos = models.BooleanField(default=False)
+
 class Account(models.Model):
     username = models.EmailField()
     django_user = models.ForeignKey(settings.AUTH_USER_MODEL, models.CASCADE, null=True)
     access_token = models.TextField(null=True, blank=True)
     client= models.ForeignKey(Client, models.SET_NULL, null=True)
+    preferences = models.ForeignKey(Preference, models.SET_NULL, null=True)
+
