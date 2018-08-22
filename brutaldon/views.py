56114c251f7dfa7ade998866b76b2530ef61461b
Jason McBrayer <jmcbray@carcosa.net>
2018-08-22 09:27:45 -0400

    Fix crash of privacy page for logged out user

--- brutaldon/views.py
+++ brutaldon/views.py
@@ -638,7 +638,7 @@ def about(request):
 def privacy(request):
     return render(request, 'privacy.html',
                       {"fullbrutalism": fullbrutalism_p(request),
-                       'own_acct' : request.session['user', None]})
+                       'own_acct' : request.session.get('user', None)})
 
 @cache_page(60 * 30)
 @br_login_required
