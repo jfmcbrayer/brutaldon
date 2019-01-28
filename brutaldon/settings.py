ef8cee932f0af08024f0a4505cad6cf2fa5e19a6
Jason McBrayer <jmcbray@carcosa.net>
2019-01-27 22:51:02 -0500

    Bump minor version because of major Pleroma-related changes

--- brutaldon/settings.py
+++ brutaldon/settings.py
@@ -194,5 +194,5 @@ SESSION_SERIALIZER = 'django.contrib.sessions.serializers.PickleSerializer'
 ANONYMOUS_HOME_URL = 'about'
 
 # Version number displayed on about page
-BRUTALDON_VERSION = "2.8.2"
+BRUTALDON_VERSION = "2.9.0"
 
