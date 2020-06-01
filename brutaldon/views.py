5187715244b1739df89ff9f6da3ca48a6851b747
Cy <autocommit>
2020-06-01 02:34:15 +0000

    Consistent naming
    Right, I forgot I was in the middle of changing the name post to toot. Thus explaining why op.post had an empty result, since op.toot was what I changed.

--- brutaldon/views.py
+++ brutaldon/views.py
@@ -692,7 +692,7 @@ def thread(request, id):
     # descendants = [
     #     x for x in context.descendants if not toot_matches_filters(x, filters)
     # ]
-    posts = tuple(threadtree.build(mastodon, context.descendants))
+    toots = tuple(threadtree.build(mastodon, context.descendants))
     return render(
         request,
         "main/thread.html",
@@ -700,7 +700,7 @@ def thread(request, id):
             "context": context,
             "toot": toot,
             "root": root,
-            "posts": posts,
+            "toots": toots,
             "own_acct": request.session["active_user"],
             "notifications": notifications,
             "preferences": account.preferences,
